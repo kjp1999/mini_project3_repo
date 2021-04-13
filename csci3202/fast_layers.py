5eeb7b93a9d0f39ec393a68d3e197a11689735e8
crheckman <crheckman@gmail.com>
2021-04-13 17:38:14 -0600

    change python2 to python3 syntax

--- csci3202/fast_layers.py
+++ csci3202/fast_layers.py
@@ -3,9 +3,9 @@ try:
   from csci3202.im2col_cython import col2im_cython, im2col_cython
   from csci3202.im2col_cython import col2im_6d_cython
 except ImportError:
-  print 'run the following from the csci3202 directory and try again:'
-  print 'python setup.py build_ext --inplace'
-  print 'You may also need to restart your iPython kernel'
+  print('run the following from the csci3202 directory and try again:')
+  print('python setup.py build_ext --inplace')
+  print('You may also need to restart your iPython kernel')
 
 from csci3202.im2col import *
 
