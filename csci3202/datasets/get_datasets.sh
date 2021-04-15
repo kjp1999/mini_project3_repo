f89b88a60564cdbb2c2e80d1165b6b8a391d7b93
crheckman <crheckman@gmail.com>
2021-04-15 14:39:35 -0600

    fix get_datasets.sh for OSX users

--- csci3202/datasets/get_datasets.sh
+++ csci3202/datasets/get_datasets.sh
@@ -1,4 +1,10 @@
 # Get CIFAR10
-wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+
+if [[ "$OSTYPE" == "darwin"* ]]; then # OS X
+  curl http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -o cifar-10-python.tar.gz
+else # Linux, Cygwin, BSD, ...
+  wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+fi
+
 tar -xzvf cifar-10-python.tar.gz
 rm cifar-10-python.tar.gz 
